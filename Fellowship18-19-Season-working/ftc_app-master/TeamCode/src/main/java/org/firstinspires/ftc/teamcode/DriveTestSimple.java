4f222ea05e71e97f19cc34957a809681cf7e664c
James Freitag <kfman18@gmail.com>
2018-10-04 21:07:32 -0700

    Tank Drive

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTestSimple.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTestSimple.java
@@ -36,20 +36,6 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
-/**
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
- * The code is structured as a LinearOpMode
- *
- * This particular OpMode executes a POV Game style Teleop for a PushBot
- * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
- * It raises and lowers the claw using the Gampad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="DriveTestSimple:", group="Pushbot")
 public class DriveTestSimple extends LinearOpMode {
 
@@ -146,7 +132,7 @@ public class DriveTestSimple extends LinearOpMode {
                 backRightPower = (backRightPower*127)/(HighValue);
             }
 
-            //telemetry.addData("","FLP: %.3f, FRP: %.3f, BLP: %.3f, BRP: %.3f", frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+            telemetry.addData("","FLP: %.3f, FRP: %.3f, BLP: %.3f, BRP: %.3f", frontLeftPower, frontRightPower, backLeftPower, backRightPower);
 
             HighValue = 127;
             frontLeftPower = (frontLeftPower*MaxPower)/(HighValue);
