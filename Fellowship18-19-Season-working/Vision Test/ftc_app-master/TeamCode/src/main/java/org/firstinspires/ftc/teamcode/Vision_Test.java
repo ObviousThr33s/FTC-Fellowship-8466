f0944fd84d85afa3dcc10a7eed8fa7c1a6ace527
James Freitag <kfman18@gmail.com>
2018-10-07 16:53:13 -0700

    Added Camera Classes

---
+++ Fellowship18-19-Season-working/Vision Test/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision_Test.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Vision Test", group="Teleop" )
+public class Vision_Test extends LinearOpMode{
+    public void runOpMode(){}
+}
