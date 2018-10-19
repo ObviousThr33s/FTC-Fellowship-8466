30ed1c50cdd6bae249b24942f8de3484b171d060
LingLing <lingyuezhang@yahoo.com>
2018-10-18 21:40:27 -0700

    make the autonomous sandbox code working

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/sandbox/PushbotAutoDriveByEncoder_Linear.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/sandbox/PushbotAutoDriveByEncoder_Linear.java
@@ -62,8 +62,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
-@Disabled
+@Autonomous(name="Samwise: Auto Drive By Encoder", group="Pushbot")
+//@Disabled
 public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
@@ -108,12 +108,12 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  6,  6, 2.5);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   1, -1, 1.5);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -3, -3, 2.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
-        robot.leftClaw.setPosition(1.0);            // S4: Stop and close the claw.
-        robot.rightClaw.setPosition(0.0);
+        //robot.leftClaw.set/Position(1.0);            // S4: Stop and close the claw.
+        //robot.rightClaw.setPosition(0.0);
         sleep(1000);     // pause for servos to move
 
         telemetry.addData("Path", "Complete");
