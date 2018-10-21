b607b27cf99851b26d452da274909e13eeee2891
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-10-21 15:02:43 -0700

    Autonomous Folder Added, Sampling is added.

---
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Sampling/sampling.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Samwise.Sampling;
+
+import org.firstinspires.ftc.teamcode.AbstractConceptual.Robot;
+import org.firstinspires.ftc.teamcode.AbstractPhysical.DriveTrain;
+import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
+import org.firstinspires.ftc.teamcode.AbstractPhysical.Sensors;
+import org.firstinspires.ftc.teamcode.AbstractPhysical.Vision;
+import org.firstinspires.ftc.teamcode.Samwise.DriveTrain.SamwiseDriveTrain;
+
+public class SamwiseSampling extends Vision {
+
+
+}
