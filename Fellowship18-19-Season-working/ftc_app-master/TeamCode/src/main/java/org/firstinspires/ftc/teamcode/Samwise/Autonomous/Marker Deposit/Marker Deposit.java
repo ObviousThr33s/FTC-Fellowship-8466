3a27c82196aa970991a7a4e13d2015943bf37de8
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-10-21 15:31:30 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/ObviousThr33s/FTC-Fellowship-8466""
    This reverts commit 199caf8fb9e6b361a79e47607514df1793d57c01.

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Marker Deposit/Marker Deposit.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Marker Deposit/Marker Deposit.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.firstinspires.ftc.teamcode.Samwise.MarkerDeposit;
 
 import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
@@ -5,3 +6,5 @@ import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
 public class SamwiseMarkerDeposit extends MotorsAndServos {
 
 }
+=======
+>>>>>>> parent of f705d7b... Packaging
