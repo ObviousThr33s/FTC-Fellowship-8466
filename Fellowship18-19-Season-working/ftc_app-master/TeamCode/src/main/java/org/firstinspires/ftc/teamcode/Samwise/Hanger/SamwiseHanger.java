7981de2fce1026154644d9ad04e3670f3132c27a
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-10-21 16:58:29 -0700

    Some Renaming

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHanger.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHanger.java
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode.Samwise.Hanger;
 
 import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
 
-public class Hanger extends MotorsAndServos {
+public class SamwiseHanger extends MotorsAndServos {
 
 }
