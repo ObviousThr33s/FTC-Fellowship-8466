d3dc2803b94fa3f66971c691053ff29953414d85
LingYue <lingyuezhang@yahoo.com>
2018-10-21 17:01:46 -0700

    Autonomous Driving

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Drive/SamwiseAutoDrive.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Drive/SamwiseAutoDrive.java
@@ -1,38 +1,11 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-package org.firstinspires.ftc.teamcode.Samwise.sandbox;
+package org.firstinspires.ftc.teamcode.Samwise.Autonomous.Drive;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Samwise.DriveTrain.*;
 
 /**
  * This file illustrates the concept of driving a path based on encoder counts.
@@ -63,13 +36,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Samwise: Auto Drive By Encoder", group="Pushbot")
 //@Disabled
-public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
+public class SamwiseAutoDrive extends LinearOpMode {
 
     /* Declare OpMode members. */
-    HardwarePushbot robot   = new HardwarePushbot();   // Use a Pushbot's hardware
-    private ElapsedTime     runtime = new ElapsedTime();
+    SamwiseDriveTrain robot   = new SamwiseDriveTrain();   // Use a drivetrain's hardware
+    private ElapsedTime runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     COUNTS_PER_MOTOR_REV    = 144 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
@@ -108,8 +81,8 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         encoderDrive(DRIVE_SPEED,  1,  1, 2.5);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   0.5, -0.5, 1.5);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -0.2, -0.2, 2.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(TURN_SPEED,   1, -1, 1.5);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -1, -1, 2.5);  // S3: Reverse 24 Inches with 4 Sec timeout
 
         //robot.leftClaw.set/Position(1.0);            // S4: Stop and close the claw.
         //robot.rightClaw.setPosition(0.0);
--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/DriveTrain/SamwiseDriveTrain.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/DriveTrain/SamwiseDriveTrain.java
@@ -7,25 +7,45 @@ import org.firstinspires.ftc.teamcode.AbstractPhysical.DriveTrain;
 
 public class SamwiseDriveTrain extends DriveTrain {
 
-    DcMotor leftMotor = null;
-    DcMotor rightMotor = null;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
 
-    public SamwiseDriveTrain(DcMotor leftMotor, DcMotor rightMotor, HardwareMap hw){
-        leftMotor = hw.dcMotor.get("leftMotor");
+    public SamwiseDriveTrain(){
     }
 
-    public  SamwiseDriveTrain(){
-        int addnumber=this.addNumbers(3, 4);
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap hwm) {
+
+        // Define and Initialize MotorsAndServos
+        leftDrive  = hwm.get(DcMotor.class, "left_drive");
+        rightDrive = hwm.get(DcMotor.class, "right_drive");
+        //leftArm    = hwMap.get(DcMotor.class, "left_arm");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        //leftArm.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        //leftClaw  = hwMap.get(Servo.class, "left_hand");
+        //rightClaw = hwMap.get(Servo.class, "right_hand");
+        //leftClaw.setPosition(MID_SERVO);
+        //rightClaw.setPosition(MID_SERVO);
     }
 
     @Override
     public void drive(double power){
-        leftMotor.setPower(power);
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
     }
 
-    private int addNumbers(int number1, int number2) {
-        int result = number1 + number2;
-        return result;
-    }
 }
 
