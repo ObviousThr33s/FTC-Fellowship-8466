d58814e8bbcff0cc3291a917a236b355f68b03f1
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2018-10-25 21:07:01 -0700

    Update Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollection.java
    started collection

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollection.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollection.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode.Samwise.Collection;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
 
 public class SamwiseCollection extends MotorsAndServos {
 
+    public DcMotor motor3 = null;
+    public DcMotor motor4 = null;
+    public DcMotor motor5 = null;
+    public DcMotor motor6 = null;
 
 
 }
