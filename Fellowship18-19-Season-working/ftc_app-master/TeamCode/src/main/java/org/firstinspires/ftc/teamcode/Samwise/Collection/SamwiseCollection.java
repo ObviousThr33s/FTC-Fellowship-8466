b3b5613866eb1df070a04f0ddd6011407cc48001
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-10-28 17:08:14 -0700

    Samwise Collection Servo Hardware Map 1.0

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollection.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollection.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode.Samwise.Collection;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
+import org.firstinspires.ftc.teamcode.Samwise.SamwiseMotors.SamwiseMotors;
 
-public class SamwiseCollection extends MotorsAndServos {
+public class SamwiseCollection extends SamwiseMotors {
 
-    public DcMotor motor3 = null;
-    public DcMotor motor4 = null;
-    public DcMotor motor5 = null;
-    public DcMotor motor6 = null;
+    public Servo servo3 = null;
+    public Servo servo4 = null;
+    public Servo servo5 = null;
+    public Servo servo6 = null;
+    public final static double servo3_HOME = 0.2;
+    public final static double servo4_HOME = 0.2;
+    public final static double servo5_HOME = 0.2;
+    public final static double servo6_HOME = 0.2;
 
+    public void init(HardwareMap ahwMap) {
+        servo3 = ahwMap.servo.get("servo3");
+        servo4 = ahwMap.servo.get("servo4");
+        servo5 = ahwMap.servo.get("servo5");
+        servo6 = ahwMap.servo.get("servo6");
+    }
 
 }
