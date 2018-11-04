4e8cfdbd4215819925402cc80d6cf16b750284c4
Kai Zhang <mkz100@gmail.com>
2018-11-04 15:41:35 -0800

    FTC 4.3 Tensorflow Sampling code

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollectionHardware.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Collection/SamwiseCollectionHardware.java
@@ -8,20 +8,11 @@ import org.firstinspires.ftc.teamcode.Samwise.SamwiseMotors.SamwiseMotors;
 
 public class SamwiseCollectionHardware {
 
-    public Servo servo1 = null; //shoulder hori.
-    public Servo servo2 = null; //shoulder vert.
-    public Servo servo3 = null; //elbow
-    public Servo servo4 = null; //wrist
-    public Servo servo5 = null; //hand
-    public Servo servo6 = null; //spring 1
-    public Servo servo7 = null; //spring 2
+    public Servo servo3 = null;
+    public Servo servo4 = null;
+    public Servo servo5 = null;
+    public Servo servo6 = null;
 
-    public final static double servo1_HOME = 0.5;
-    public final static double servo1_MIN_RANGE = 0.2;
-    public final static double servo1_MAX_RANGE = 0.7;
-    public final static double servo2_HOME = 0.5;
-    public final static double servo2_MIN_RANGE = 0.2;
-    public final static double servo2_MAX_RANGE = 0.7;
     public final static double servo3_HOME = 0.5;
     public final static double servo3_MIN_RANGE = 0.2;
     public final static double servo3_MAX_RANGE = 0.7;
@@ -34,27 +25,18 @@ public class SamwiseCollectionHardware {
     public final static double servo6_HOME = 0.5;
     public final static double servo6_MIN_RANGE = 0.2;
     public final static double servo6_MAX_RANGE = 0.7;
-    public final static double servo7_HOME = 0.5;
-    public final static double servo7_MIN_RANGE = 0.2;
-    public final static double servo7_MAX_RANGE = 0.7;
 
     public void init(HardwareMap ahwMap) {
         //Define names on the phone
-        servo1 = ahwMap.servo.get("servo_1");
-        servo2 = ahwMap.servo.get("servo_2");
         servo3 = ahwMap.servo.get("servo_3");
         servo4 = ahwMap.servo.get("servo_4");
         servo5 = ahwMap.servo.get("servo_5");
         servo6 = ahwMap.servo.get("servo_6");
-        servo7 = ahwMap.servo.get("servo_7");
 
-        servo1.setPosition(servo1_HOME);
-        servo2.setPosition(servo2_HOME);
         servo3.setPosition(servo3_HOME);
         servo4.setPosition(servo4_HOME);
         servo5.setPosition(servo5_HOME);
         servo6.setPosition(servo6_HOME);
-        servo7.setPosition(servo7_HOME);
     }
 
 }
