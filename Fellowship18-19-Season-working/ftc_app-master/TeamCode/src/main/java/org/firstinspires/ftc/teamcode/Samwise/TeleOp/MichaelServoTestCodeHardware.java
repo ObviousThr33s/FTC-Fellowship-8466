b1d396fb618c2633f8ff548c577f0f64f6b89621
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2018-11-04 17:57:35 -0800

    fixed stuff in test code

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeHardware.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeHardware.java
@@ -5,17 +5,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class MichaelServoTestCodeHardware {
     public Servo testServo1 = null;
-    public Servo testServo2 = null;
 
     public static double testServo1_HOME = 0.5;
-    public static double testServo2_HOME = 0.5;
 
     public void init(HardwareMap ahwMap) {
         testServo1 = ahwMap.servo.get("testservo_1");
-        testServo2 = ahwMap.servo.get("testservo_2");
 
         testServo1.setPosition(testServo1_HOME);
-        testServo2.setPosition(testServo2_HOME);
     }
 
 }
--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeTeleop.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeTeleop.java
@@ -4,13 +4,11 @@ package org.firstinspires.ftc.teamcode.Samwise.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Michael's Servo test")
+@TeleOp(name = "Michael's Servo Test2")
 public class MichaelServoTestCodeTeleop extends LinearOpMode {
     MichaelServoTestCodeHardware testTeleOp = new MichaelServoTestCodeHardware();
     double testServo1Pos = testTeleOp.testServo1_HOME;
-    double testServo2Pos = testTeleOp.testServo2_HOME;
     final double testServo1_SPEED = 0.01;
-    final double testServo2_SPEED = 0.01;
 
     @Override
 
@@ -18,11 +16,15 @@ public class MichaelServoTestCodeTeleop extends LinearOpMode {
         testTeleOp.init(hardwareMap);
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.a)
+            if (gamepad1.a) {
                 testServo1Pos += testServo1_SPEED;
-            else if (gamepad1.b)
+            } else {
+                testServo1Pos = 0;
+            }
+            if (gamepad1.b) {
                 testServo1Pos -= testServo1_SPEED;
-
+            } else
+                testServo1Pos = 0;
             testTeleOp.testServo1.setPosition(testServo1Pos);
 
             telemetry.addData("test", "%.2f", testServo1Pos);
