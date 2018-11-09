ca86881fb5a832fa81a08dc595b2a4f9982fb1a3
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2018-11-08 20:11:45 -0800

    updated "finished" test code

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeTeleop.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelServoTestCodeTeleop.java
@@ -4,11 +4,11 @@ package org.firstinspires.ftc.teamcode.Samwise.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Michael's Servo Test2")
+@TeleOp(name = "Michael's Servo Test212l")
 public class MichaelServoTestCodeTeleop extends LinearOpMode {
     MichaelServoTestCodeHardware testTeleOp = new MichaelServoTestCodeHardware();
     double testServo1Pos = testTeleOp.testServo1_HOME;
-    final double testServo1_SPEED = 0.01;
+    final double testServo1_SPEED = 0.06;
 
     @Override
 
@@ -16,20 +16,18 @@ public class MichaelServoTestCodeTeleop extends LinearOpMode {
         testTeleOp.init(hardwareMap);
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.a) {
-                testServo1Pos += testServo1_SPEED;
-            } else {
-                testServo1Pos = 0;
+            if (gamepad1.x) {
+                testServo1Pos = 0.3;
+            }else {
+                testServo1Pos = 0.7;
             }
-            if (gamepad1.b) {
-                testServo1Pos -= testServo1_SPEED;
-            } else
-                testServo1Pos = 0;
+
+
+
             testTeleOp.testServo1.setPosition(testServo1Pos);
 
             telemetry.addData("test", "%.2f", testServo1Pos);
 
-            sleep(40);
         }
     }
 }
