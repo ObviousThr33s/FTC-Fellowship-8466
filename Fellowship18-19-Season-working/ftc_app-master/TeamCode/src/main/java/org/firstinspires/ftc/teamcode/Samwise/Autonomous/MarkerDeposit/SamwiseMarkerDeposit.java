2e12e0f824ca31f872e7ec87416f6b94cc89501d
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-11-15 19:40:12 -0800

    Marker Deposit weird thingie happen

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/MarkerDeposit/SamwiseMarkerDeposit.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/MarkerDeposit/SamwiseMarkerDeposit.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode.Samwise.Autonomous.MarkerDeposit;
 
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
 
 public class SamwiseMarkerDeposit extends MotorsAndServos {
 
+    public Servo servo9 = null;
+
+    public void init() {
+        servo9 = HardwareMap.Servo.get("Servo9");
+
+        servo9 setPosition(0.0);
+    }
 }
--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/SamwiseMotors/SamwiseMotors.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/SamwiseMotors/SamwiseMotors.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.Samwise.SamwiseMotors;
 
 public class SamwiseMotors {
+
+    public class Hwm = null
 }
