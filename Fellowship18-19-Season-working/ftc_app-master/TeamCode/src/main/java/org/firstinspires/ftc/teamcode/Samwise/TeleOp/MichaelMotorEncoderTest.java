5237fb2dcabb0db9e307863ec6cb11213814cf1a
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2018-11-15 20:51:06 -0800

    Update MichaelMotorEncoderTest.java

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelMotorEncoderTest.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelMotorEncoderTest.java
@@ -15,13 +15,21 @@ public class MichaelMotorEncoderTest extends OpMode {
     public void init() {
         testMotorEncoder1 = hardwareMap.dcMotor.get("testMotor1");
 
-        
+        testMotorEncoder1.setPower(0);
+
+        testMotorEncoder1.setDirection(DcMotor.Direction.REVERSE);
 
 
     }
 
     public void loop() {
-
+        if (gamepad1.x) {
+            testMotorEncoder1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            float testrightturn90 = TestEncoderTIckCount/4;
+            testMotorEncoder1.setTargetPosition((int)testrightturn90);
+            testMotorEncoder1.setPower(0.8);
+            testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
     }
 
 }
