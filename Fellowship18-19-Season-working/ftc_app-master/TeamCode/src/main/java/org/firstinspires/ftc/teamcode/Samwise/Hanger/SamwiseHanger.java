bf1ceee969a5fcd03fd67a4900eda25f7076feb5
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-11-15 19:55:30 -0800

    HangerHardware
    Lil Skeeet

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHanger.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.Samwise.Hanger;
-
-import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
-
-public class SamwiseHanger extends MotorsAndServos {
-
-}
---
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHangerHardware.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Samwise.Hanger;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
+import org.firstinspires.ftc.teamcode.Samwise.SamwiseMotors.SamwiseMotors;
+
+@Autonomous(name="SamwiseHangerHardware", group="Exercises")
+public class SamwiseHangerHardware extends LinearOpMode {
+
+    public DcMotor hangermotor1 = null;
+
+    public Servo hangerservo1 = null;
+
+    public void init(HardwareMap ahwMap) {
+        hangermotor1 = ahwMap.dcMotor.get("hangermotor_1");
+        hangerservo1 = ahwMap.servo.get("hangerservo_1");
+    }
+    public void runOpMode() {
+
+            hangermotor1.setDirection(DcMotor.Direction.REVERSE);
+
+            hangerservo1.setPosition(0.0);
+
+            telemetry.addData("Mode", "GGwp");
+            telemetry.update();
+
+            waitForStart(); {
+
+                telemetry.addData("Mode", "DoubleBoys");
+                telemetry.update();
+
+                hangermotor1.setPower(0.00000000025);
+
+                sleep(1000000000);
+
+                hangermotor1.setPower(0.0);
+
+                hangerservo1.setPosition(1);
+
+                sleep(1000000000);
+
+                hangerservo1.setPosition(0.0);
+            }
+        }
+    }
+}
