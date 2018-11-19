1a0afc1dfa35538c7ef495e8b288ed84cf7311ae
RiptideStar <42722242+RiptideStar@users.noreply.github.com>
2018-11-18 16:09:05 -0800

    Marker Deposit

---
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/MarkerDeposit/KyleServoTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Samwise.Autonomous.MarkerDeposit;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Samwise.TeleOp.MichaelServoTestCodeHardware;
+
+@TeleOp(name = "kyle's Servo Test")
+public class KyleServoTest extends LinearOpMode {
+    SamwiseMarkerDeposit md = new SamwiseMarkerDeposit();
+    final double testServo1_SPEED = 0.04;
+
+    @Override
+    public void runOpMode() {
+        md.init(hardwareMap);
+        waitForStart();
+        //while (opModeIsActive()) {
+            md.move(1);
+            sleep(2000);
+            md.move(0);
+            //telemetry.addData("test", "%.2f",md);
+            //telemetry.update();
+        //}
+    }
+}
