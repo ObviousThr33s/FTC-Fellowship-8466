7d4584b37cfde34ade17ab3a915c718aeb4b00cb
Kai Zhang <mkz100@gmail.com>
2018-11-20 17:49:35 -0800

    Update Crater Center

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Drive/SamwiseDriveRouteCraterCenter.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Autonomous/Drive/SamwiseDriveRouteCraterCenter.java
@@ -27,19 +27,19 @@ public class SamwiseDriveRouteCraterCenter extends SamwiseAutoDriveWithTensorflo
         telemetryNow(route, "starting parent common drive ...");
         super.drive();
 
-        encoderDrive(DRIVE_SPEED, 26, 26, 5);
+        encoderDrive(DRIVE_SPEED, 22, 22, 5);
         telemetry.addData(route, "finish second drive");
         telemetry.update();
 
-        encoderDrive(DRIVE_SPEED, -8, -8, 4);
+        encoderDrive(DRIVE_SPEED, -8, -7, 4);
         telemetry.addData(route, "finish third drive");
         telemetry.update();
 
-        turnDrive(TurnDirection.RIGHT, 65, 5);
+        turnDrive(TurnDirection.RIGHT, 90, 5);
         telemetry.addData(route, "finish fourth turn");
         telemetry.update();
 
-        encoderDrive(DRIVE_SPEED, -39,-39,20);
+        encoderDrive(DRIVE_SPEED, -45,-45,20);
         telemetry.addData(route, "finish fifth drive");
         telemetry.update();
 
@@ -51,7 +51,9 @@ public class SamwiseDriveRouteCraterCenter extends SamwiseAutoDriveWithTensorflo
         telemetry.addData(route, "finish seventh drive");
         telemetry.update();
 
-        encoderDrive(DRIVE_SPEED, 69, 69,20);
+        md.move(1);
+
+        encoderDrive(DRIVE_SPEED, 68, 68,20);
         telemetry.addData(route, "finish eighth drive");
         telemetry.update();
 
