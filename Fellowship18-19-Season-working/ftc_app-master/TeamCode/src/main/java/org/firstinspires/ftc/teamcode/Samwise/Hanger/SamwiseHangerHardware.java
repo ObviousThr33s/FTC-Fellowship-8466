ce08eae0fe3ca8179e96e594bacf22bc11a53db7
Kai Zhang <mkz100@gmail.com>
2018-11-29 20:52:36 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/ObviousThr33s/FTC-Fellowship-8466""
    This reverts commit e5ed1ca79705dc08e65c5d51894385f7c538c78e.

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHangerHardware.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/Hanger/SamwiseHangerHardware.java
@@ -1,49 +1,35 @@
 package org.firstinspires.ftc.teamcode.Samwise.Hanger;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.teamcode.AbstractPhysical.MotorsAndServos;
-import org.firstinspires.ftc.teamcode.Samwise.SamwiseMotors.SamwiseMotors;
 
-@Autonomous(name="SamwiseHangerHardware", group="Exercises")
-public class SamwiseHangerHardware extends LinearOpMode {
+public class SamwiseHangerHardware {
 
-    public DcMotor hangermotor1 = null;
+    public DcMotor hangermotor1;
+    public Servo hangerservo1;
+    public boolean hookd;
 
-    public Servo hangerservo1 = null;
-
-    public void init(HardwareMap ahwMap) {
-        hangermotor1 = ahwMap.dcMotor.get("hangermotor_1");
-        hangerservo1 = ahwMap.servo.get("hangerservo_1");
+    public SamwiseHangerHardware(HardwareMap hw){
+        hangermotor1 = hw.dcMotor.get("hangermotor_1");
+        hangerservo1 = hw.servo.get("hangerservo_1");
     }
-    public void runOpMode() {
 
-            hangermotor1.setDirection(DcMotor.Direction.REVERSE);
-
-            hangerservo1.setPosition(0.0);
-
-            telemetry.addData("Mode", "GGwp");
-            telemetry.update();
-
-            waitForStart(); {
-
-                telemetry.addData("Mode", "DoubleBoys");
-                telemetry.update();
-
-                hangermotor1.setPower(0.00000000025);
-
-                sleep(1000000000);
-
-                hangermotor1.setPower(0.0);
-
-                hangerservo1.setPosition(1);
-
-                sleep(1000000000);
-
-                hangerservo1.setPosition(0.0);
-            }
+    public void moveDown(){
+        if (hookd == true){
+            //Move until robot is down
+        }
+        if (hookd == false){
+            //Dont move the hanger
         }
     }
+
+    public void unHook(){
+        if (hookd == true){
+            //Unhook the robot
+        }
+        if (hookd == false){
+            //Dont unhook the robot
+        }
+    }
+}
