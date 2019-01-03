62c8a2d5c74a5963796e323b2b1f67e61dad8ae8
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2019-01-02 23:27:10 -0800

    Updated some not very useful code but here it is

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelMotorEncoderTest2.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/MichaelMotorEncoderTest2.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp (name = "EncodertestOftheMichael")
+@TeleOp (name = "FinalMichaelArmTest")
 public class MichaelMotorEncoderTest2 extends OpMode {
 
     private DcMotor testMotorEncoder1 = null;
@@ -21,33 +21,29 @@ public class MichaelMotorEncoderTest2 extends OpMode {
     static final int SmallGearCount = 24;
     static final int EncoderCountJ1 = 1680;
     static final int MaxOrMinDegrees = 45;
-    private int J1GearRatio = BigGearCount/SmallGearCount;
-    private int TickPerDegreeJ1 = EncoderCountJ1/360;
+    private int J1GearRatio = BigGearCount / SmallGearCount;
+    private int TickPerDegreeJ1 = EncoderCountJ1 / 360;
     private int BigToSmallRatio = MaxOrMinDegrees * J1GearRatio;
     private int SmallDegreeToTicks = BigToSmallRatio * TickPerDegreeJ1;
-//    private Servo ArmServo1 = null;
- //   private Servo ArmServo2 = null;
-  //  private Servo HandServo1 = null;
-  //  private Servo HandServo2 = null;
+
+
     //for J2 and J3
-    static final int EncoderCountJ2 = 1120; //number of ticks per motor round
+    static final double EncoderCountJ2 = 1120.0; //number of ticks per motor round
 
-    static final int EncoderCountJ3 = 1120; //number of ticks per motor round
-    static final int HeightOfPlane = 2; //height of plane of motion
-    static final int LengthJ2toJ3 = 6; //distance between J2 and J3
-    static final int LengthJ3toJ4 = 6; //distance between J3 and J4
+    static final double EncoderCountJ3 = 1120.0; //number of ticks per motor round
+    static final double HeightOfPlane = 2.0; //height of plane of motion
+    static final double LengthJ2toJ3 = 6.0; //distance between J2 and J3
+    static final double LengthJ3toJ4 = 6.0; //distance between J3 and J4
 
-    private int H = HeightOfPlane;
-    private int L1 = LengthJ2toJ3; //length between J2 and J3
-    private int L2 = LengthJ3toJ4; //length between J3 and J4
-    private float TickPerDegreeJ3 = EncoderCountJ3/360;
-    private float TickPerDegreeJ2 = EncoderCountJ2/360;
-    //private double omega = Math.asin(H + Math.cos (180 - theta) * L1) / L2;
-    //private double r = L1 * Math.sin (180 - theta) + L2 * Math.cos(Math.asin( H + Math.cos(180 - theta) * L1) / L2);
+    private double H = HeightOfPlane;
+    private double L1 = LengthJ2toJ3; //length between J2 and J3
+    private double L2 = LengthJ3toJ4; //length between J3 and J4
+    private double TickPerDegreeJ3 = EncoderCountJ3 / 360.0;
+    private double TickPerDegreeJ2 = EncoderCountJ2 / 360.0;
+
     private double theta = 0;  // rotate degree of J2 if
     private double phi = 0; //Rotate J3
-    //private double theta2ticks = theta * TickPerDegreeJ2;
-  //  private double phi2ticks = phi * TickPerDegreeJ3;
+
 
     public void init() {
 
@@ -71,19 +67,20 @@ public class MichaelMotorEncoderTest2 extends OpMode {
         testMotorEncoder2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         testMotorEncoder3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //testMotorEncoder4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // initialize Arm to a default potition,
-        double theta_init = 90;
-        double phi_init = Math.acos((L1 * Math.cos(180-theta_init)-H)/L2)+180-theta_init;
+        // initialize Arm to a default position, (temporary code)
+        double theta_init = 90.0;
+        double phi_init = Math.acos((L1 * Math.cos(180.0 - theta_init) - H) / L2) + 180.0 - theta_init;
 
-        testMotorEncoder1.setTargetPosition((int) (theta_init * TickPerDegreeJ2) );
-        testMotorEncoder2.setTargetPosition((int) (phi_init * TickPerDegreeJ3) );
+        testMotorEncoder1.setTargetPosition((int) (theta_init * TickPerDegreeJ2));
+        testMotorEncoder2.setTargetPosition((int) (phi_init * TickPerDegreeJ3));
         testMotorEncoder2.setPower(0.5);
         testMotorEncoder1.setPower(0.5);
         testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         testMotorEncoder2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         theta = theta_init;
     }
-
+/*
+    // Test Loop1 control J2. J3 by one key
     public void loop() {
         //print out the current motor postion for verifying initial postion settings
 
@@ -96,44 +93,142 @@ public class MichaelMotorEncoderTest2 extends OpMode {
         float RotationJoint = gamepad1.right_stick_x * SmallDegreeToTicks; // rotation of J1
 
         testMotorEncoder3.setTargetPosition((int)RotationJoint); //Joint 1 (turn table)
+        testMotorEncoder3.setPower(0.8);
+
+        double a = Math.cos(Math.toRadians(180.0-theta));
+        double b = (L1 * a - H)/L2;
+        double c = Math.acos(b);
+        double c_1 = Math.toDegrees(c);
+        double d = c + 180.0 - theta;
+        //phi = Math.acos((L1 * Math.cos(180.0-theta)-H)/L2)+180.0-theta;
+        phi = d;
+
+        double thetatoticks = theta * TickPerDegreeJ2;
+        double phitoticks = phi * TickPerDegreeJ3;
+
+        if (gamepad1.left_stick_y < -0.1) {
+            if (theta > 90){
+            //if ((theta >= 90)&&(theta <180)){
+                testMotorEncoder1.setTargetPosition((int) thetatoticks);
+                testMotorEncoder2.setTargetPosition((int) phitoticks);
+                if ((!testMotorEncoder1.isBusy())&& (!testMotorEncoder2.isBusy())) {
+                    testMotorEncoder1.setPower(0.8);
+                    testMotorEncoder2.setPower(0.8);
+                    theta = theta - (gamepad1.left_stick_y * 25);
+                };
+
+            }
+        }
+        if (gamepad1.left_stick_y >0.1) {
+            //if ((theta <= 180)&&(theta > 90)){
+
+            if (theta < 180){
+                testMotorEncoder1.setTargetPosition((int)thetatoticks);
+                testMotorEncoder2.setTargetPosition((int)phitoticks);
+                if ((!testMotorEncoder1.isBusy())&& (!testMotorEncoder2.isBusy())) {
+                    testMotorEncoder1.setPower(0.8);
+                    testMotorEncoder2.setPower(0.8);
+                    theta = theta - (gamepad1.left_stick_y * 25);
+
+                }
+            }
+
+        }
+        if (gamepad1.left_stick_y == 0) {
+            testMotorEncoder1.setPower(0);
+            testMotorEncoder2.setPower(0);
+        }
 
 
-        phi = Math.acos((L1 * Math.cos(180-theta)-H)/L2)+180-theta;
+        System.out.println("===>" + phitoticks + "phi to ticks");
+        System.out.println("===>" + thetatoticks +"theta to ticks");
+
+
+
+        testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        testMotorEncoder2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        testMotorEncoder3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+    public void stop() {
+        testMotorEncoder1.setPower(0);
+        testMotorEncoder2.setPower(0);
+        testMotorEncoder3.setPower(0);
+    }
+
+}
+*/
+//Test Loop 2; control J2 and J3 separately
+
+    public void loop() {
+        //print out the current motor postion for verifying initial postion settings
+
+        int testMotor1Pos = testMotorEncoder1.getCurrentPosition();
+        int testMotor2Pos = testMotorEncoder2.getCurrentPosition();
+        int testMotor3Pos = testMotorEncoder3.getCurrentPosition();
+
+        System.out.println(testMotor1Pos + testMotor2Pos + testMotor3Pos);
+
+        float RotationJoint = gamepad1.right_stick_x * SmallDegreeToTicks; // rotation of J1
+
+        testMotorEncoder3.setTargetPosition((int) RotationJoint); //Joint 1 (turn table)
+        testMotorEncoder3.setPower(0.8);
+
+        double a = Math.cos(Math.toRadians(180.0 - theta));
+        double b = (L1 * a - H) / L2;
+        double c = Math.acos(b);
+        double c_1 = Math.toDegrees(c);
+        double d = c + 180.0 - theta;
+        phi = d;
+        //phi = Math.acos((L1 * Math.cos(180.0-theta)-H)/L2)+180.0-theta;
         double thetatoticks = theta * TickPerDegreeJ2;
         double phitoticks = phi * TickPerDegreeJ3;
         // set power according to theta
         if (gamepad1.left_stick_y < -0.1) {
-            if( theta < 180){
+            if (theta > 90) {
+                //if ((theta >= 90)&&(theta <180)){
                 testMotorEncoder1.setTargetPosition((int) thetatoticks);
-                testMotorEncoder2.setTargetPosition((int) phitoticks);
-                testMotorEncoder1.setPower(0);
-                testMotorEncoder2.setPower(0.8);
 
-                theta = theta - (gamepad1.left_stick_y * 10);
+                if (!testMotorEncoder1.isBusy()) {
+                    testMotorEncoder1.setPower(0.8);
+                    testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    theta = theta - (gamepad1.left_stick_y * 25);
+                }
+                ;
+
+            }
+        }
+        if (gamepad1.left_stick_y > 0.1) {
+            //if ((theta <= 180)&&(theta > 90)){
+            if (theta < 180) {
+                testMotorEncoder1.setTargetPosition((int) thetatoticks);
+
+                if (!testMotorEncoder1.isBusy()) {
+                    testMotorEncoder1.setPower(0.8);
+                    testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    theta = theta - (gamepad1.left_stick_y * 25);
+                }
             }
 
-
-
-
         }
-        if (gamepad1.left_stick_y >0.1) {
-            if (theta > 90){
-                testMotorEncoder1.setTargetPosition((int)thetatoticks);
-                testMotorEncoder2.setTargetPosition((int)phitoticks);
-                testMotorEncoder1.setPower(0);
+
+        if ((gamepad1.left_stick_y == 0) && (gamepad1.x)) {
+            testMotorEncoder2.setTargetPosition((int) phitoticks);
+            while (!testMotorEncoder2.isBusy()) {
                 testMotorEncoder2.setPower(0.8);
-
-                theta = theta + (gamepad1.left_stick_y * 10);
+                testMotorEncoder2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
-
         }
-        testMotorEncoder1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        testMotorEncoder2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        System.out.println("===>" + phitoticks + "phi to ticks");
+        System.out.println("===>" + thetatoticks + "theta to ticks");
+
 
         testMotorEncoder3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
     }
+
     public void stop() {
         testMotorEncoder1.setPower(0);
         testMotorEncoder2.setPower(0);
