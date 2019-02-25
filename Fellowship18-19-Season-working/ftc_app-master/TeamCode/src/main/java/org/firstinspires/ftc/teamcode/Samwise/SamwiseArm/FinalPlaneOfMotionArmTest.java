377d19f138f312de7a1a855b27294855ca66b60d
michaelzhang <42399283+ssourgrape@users.noreply.github.com>
2019-02-24 23:20:23 -0800

    Update FinalPlaneOfMotionArmTest.java

--- Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/SamwiseArm/FinalPlaneOfMotionArmTest.java
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/SamwiseArm/FinalPlaneOfMotionArmTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.sun.source.tree.IfTree;
+
 
 
 @TeleOp (name = "PlaneOfMotion")
@@ -46,8 +46,8 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
     private double L2 = LengthJ3toJ4; //length between J3 and J4
     private double TickPerDegreeJ3 = EncoderCountJ3 / 360.0;
     private double TickPerDegreeJ2 = EncoderCountJ2 / 360.0;
-    double J2MaxPos = 90.0 * TickPerDegreeJ2;
-    double J2MinPos = 0.0 * TickPerDegreeJ2;
+    double J2MaxPos = 0;
+    double J2MinPos = 0;
 
     double J2FirsttoLast = 1;
     double J3FirsttoLast = 1;
@@ -55,8 +55,9 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
     int HoldPosONOFF = 0;
     double power_level = 0.2;
     private boolean firsttime = false;
-    double J2initialposition;
-    double J3initialposition;
+    double J2initialposition = 0;
+    double J3initialposition = 0;
+    int loopcount = 0;
 
 
     public void init() {
@@ -67,15 +68,16 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
         ArmMotor2.setPower(0);
         ArmMotor2_2.setPower(0);
         ArmMotor3.setPower(0);
-        // ArmMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // ArmMotor2_2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        // ArmMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmMotor2_2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         ArmMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         ArmMotor2_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         ArmMotor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void init_loop() {
+        loopcount = 0;
 
     /*
         int motor2currentpos;
@@ -109,32 +111,10 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
         }
         ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         */
-        ArmMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        ArmMotor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        ArmMotor2_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //manually adjust
-        if (gamepad1.left_stick_y > 0.1) {
-            ArmMotor2.setPower(power_level);
-            ArmMotor2_2.setPower(power_level);
-        } else if (gamepad1.left_stick_y < -0.1) {
-            ArmMotor2.setPower(-power_level);
-            ArmMotor2_2.setPower(-power_level);
-        } else {
-            ArmMotor2.setPower(0);
-            ArmMotor2_2.setPower(0);
-        }
-        if (gamepad1.right_stick_y > 0.1) {
-            ArmMotor3.setPower(0.5);
-        } else if (gamepad1.right_stick_y < -0.1) {
-            ArmMotor3.setPower(-0.5);
-        } else {
-            ArmMotor3.setPower(0.0);
-        }
         //initial position fixed
 
-        J2initialposition = ArmMotor2.getCurrentPosition();
-        J3initialposition = ArmMotor3.getCurrentPosition();
+        //J2initialposition = ArmMotor2.getCurrentPosition();
+        //J3initialposition = ArmMotor3.getCurrentPosition();
 
       /*  if (gamepad1.right_bumper) {
             firsttime = true;
@@ -186,9 +166,9 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
         }
 
         if (manualmode ==true) {
-            ArmMotor2_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            ArmMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            ArmMotor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+          //  ArmMotor2_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+          //  ArmMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+          //  ArmMotor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             if (gamepad1.left_stick_y>0.05) {
                 ArmMotor2.setPower(0.2);
                 ArmMotor2_2.setPower(0.2);
@@ -211,46 +191,51 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
                 ArmMotor3.setPower(0);
             }
 
-        } if (manualmode == false){
+        } if (manualmode == false) {
 
-            J2initialposition = ArmMotor2.getCurrentPosition();
-            J3initialposition = ArmMotor3.getCurrentPosition();
-            if (Math.abs(gamepad1.left_stick_y) > 0.1) {
 
-                if (gamepad1.left_stick_y <= -0.05) { //push forward
-                    J2MaxPos = J2initialposition + EncoderCountJ2 / 50.0;
-                    ArmMotor2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
-                    ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
-                    //    ArmMotor2.setPower(gamepad1.left_stick_y / 5.0);
-                    //    ArmMotor2_2.setPower(gamepad1.left_stick_y / 5.0);
-                    ArmMotor2.setPower(0.2);
-                    ArmMotor2_2.setPower(0.2);
-                    ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    J3TargetPos_Ticks = J3initialposition + (-1.47 * (EncoderCountJ2 / 50.0));
-                    ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
-                    ArmMotor3.setPower(0.5); //test Power LAtEr yeet
-                    ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-                } else if (gamepad1.left_stick_y >= 0.05) { //backwards
-                    J2MaxPos = J2initialposition - EncoderCountJ2 / 50.0;
-                    ArmMotor2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
-                    ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
-                    ArmMotor2.setPower(0.2);
-                    ArmMotor2_2.setPower(0.2);
-                    ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                while (ArmMotor2.isBusy() || ArmMotor2_2.isBusy() || ArmMotor3.isBusy()) {
 
-                    J3TargetPos_Ticks = J3initialposition - (-1.47 * (EncoderCountJ2 / 50.0));
-                    ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
-                    ArmMotor3.setPower(0.2); //test Power LAtEr yeet
-                    ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                } if ((gamepad1.left_stick_y >-0.05)&&(gamepad1.left_stick_y<0.05)) {
-                    ArmMotor2.setTargetPosition();
-                    ArmMotor2.setPower(0);
-                    ArmMotor3.setPower(0);
-                    ArmMotor2_2.setPower(0);
                 }
+                ;
+
+                J2initialposition = ArmMotor2.getCurrentPosition();
+                J3initialposition = ArmMotor3.getCurrentPosition();
+                System.out.println("======" + J2initialposition + "   " + J3initialposition);
+                if ((Math.abs(gamepad1.left_stick_y) > 0.1)&& (loopcount == 0)) {
+
+
+                    if (gamepad1.left_stick_y <= -0.05) { //push forward
+                        J2MaxPos = J2initialposition + EncoderCountJ2 / 50.0;
+                        ArmMotor2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                        ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                        //    ArmMotor2.setPower(gamepad1.left_stick_y / 5.0);
+                        //    ArmMotor2_2.setPower(gamepad1.left_stick_y / 5.0);
+                        ArmMotor2.setPower(0.1);
+                        ArmMotor2_2.setPower(0.1);
+                        ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        J3TargetPos_Ticks = J3initialposition + (-1.47 * (EncoderCountJ2 / 100.0));
+                        ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
+                        ArmMotor3.setPower(0.1); //test Power LAtEr yeet
+                        ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        loopcount += 1;
+
+                    } else if (gamepad1.left_stick_y >= 0.05) { //backwards
+                        J2MaxPos = J2initialposition - EncoderCountJ2 / 100.0;
+                        ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                        ArmMotor2.setPower(0.1);
+                        ArmMotor2_2.setPower(0.1);
+                        ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                        J3TargetPos_Ticks = J3initialposition - (-1.47 * (EncoderCountJ2 / 50.0));
+                        ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
+                        ArmMotor3.setPower(0.1); //test Power LAtEr yeet
+                        ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        loopcount += 1;
+                    }
 
 
         /*        double J2CurrentPos_deg;
@@ -286,15 +271,64 @@ public class FinalPlaneOfMotionArmTest extends OpMode {
 
                 }
                 */
+                } else {
+                    ArmMotor2.setPower(0);
+                    ArmMotor3.setPower(0);
+                    ArmMotor2_2.setPower(0);
+                    loopcount = 0;
+                }
             }
+                //busy wait
         }
 
-
-
-    }
-    public void stop(){
+    public void stopJ2J3(){
         ArmMotor2.setPower(0);
         ArmMotor2_2.setPower(0);
         ArmMotor3.setPower(0);
     }
+    public void planeofmotion1(double Joysticks){
+        double J3TargetPos_Ticks;
+
+        J2initialposition = ArmMotor2.getCurrentPosition();
+        J3initialposition = ArmMotor3.getCurrentPosition();
+        System.out.println("======" + J2initialposition + "   " + J3initialposition);
+        if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+
+            if (gamepad1.left_stick_y <= -0.05) { //push forward
+                J2MaxPos = J2initialposition + EncoderCountJ2 / 50.0;
+                ArmMotor2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                //    ArmMotor2.setPower(gamepad1.left_stick_y / 5.0);
+                //    ArmMotor2_2.setPower(gamepad1.left_stick_y / 5.0);
+                ArmMotor2.setPower(0.2);
+                ArmMotor2_2.setPower(0.2);
+                ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                J3TargetPos_Ticks = J3initialposition + (-1.47 * (EncoderCountJ2 / 50.0));
+                ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
+                ArmMotor3.setPower(0.5); //test Power LAtEr yeet
+                ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            } else if (gamepad1.left_stick_y >= 0.05) { //backwards
+                J2MaxPos = J2initialposition - EncoderCountJ2 / 50.0;
+                ArmMotor2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                ArmMotor2_2.setTargetPosition((int) (J2MaxPos * J2FirsttoLast));
+                ArmMotor2.setPower(0.2);
+                ArmMotor2_2.setPower(0.2);
+                ArmMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ArmMotor2_2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                J3TargetPos_Ticks = J3initialposition - (-1.47 * (EncoderCountJ2 / 50.0));
+                ArmMotor3.setTargetPosition((int) (J3TargetPos_Ticks * J3FirsttoLast));
+                ArmMotor3.setPower(0.2); //test Power LAtEr yeet
+                ArmMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            loopcount += 1;
+
+        }else {
+                stopJ2J3();
+                loopcount = 0;
+            }
+    }
+
 }
