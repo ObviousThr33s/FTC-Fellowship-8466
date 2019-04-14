c5e660596757a897c5ff6cc0d7b7bf3e25f73e12
Kai Zhang <mkz100@gmail.com>
2019-04-14 08:40:35 -0700

    servo testing

---
+++ Fellowship18-19-Season-working/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Samwise/TeleOp/SamwiseTeleOpTestServo.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Samwise.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Samwise.SamwiseArm.SamwiseGenius;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "Samwise Teleop", group = "Samwise")
+//@Disabled
+
+public class SamwiseTeleOpTestServo extends SamwiseTeleOp3 {
+    private SamwiseGenius armStuff;
+
+    private ElapsedTime runTime;
+
+    private boolean j4Toggle = true;
+
+    @Override
+    public void init() {
+        super.init();
+        armStuff = new SamwiseGenius(hardwareMap);
+        runTime = new ElapsedTime();
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+
+        if (gamepad2.x) {
+            if (j4Toggle) {
+                armStuff.moveJ4Up();
+                j4Toggle = false;
+            } else {
+                armStuff.moveJ4Down();
+                j4Toggle = true;
+            }
+        }
+        if (gamepad2.y) {
+            armStuff.stopJ4();
+        }
+    }
+}
